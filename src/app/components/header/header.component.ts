fc3cfd52d22f8a8ab7a68b106c882d45c70d3f43
César Casillas <cesarcasillascespedes@gmail.com.com>
2024-03-04 16:22:50 -0600

    Cambios/funciona

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent  implements OnInit {
-
+  selected : string = '';
   constructor(private router : Router) { }
 
   ngOnInit() {}
