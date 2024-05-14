414a663bc7c1083c520b4c7fc8d69c40ea891148
César Casillas <cesarcasillascespedes@gmail.com.com>
2024-05-14 09:18:30 -0600

    mejoras en diseño

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -11,7 +11,7 @@ export class HeaderComponent  implements OnInit {
 
   constructor(private router : Router) { }
   selected : string = '';
-  breadcrumbs : Array<Breadcrumbs> = [];
+  breadcrumbs : Array<Breadcrumbs> = [{label:"inicio",path:"/inicio"}];
 
   ngOnInit() {}
 
